0b388d884c7ed3753fb08383da36539093b566d6
jimcai <1290414968@qq.com>
2018-05-09 22:01:12 +0800

    spring 模拟完善2.0-事务逻辑

--- src/main/java/framework/aop/AopProxy.java
+++ src/main/java/framework/aop/AopProxy.java
@@ -8,6 +8,12 @@ import java.lang.reflect.Proxy;
 public class AopProxy implements InvocationHandler{
 
     private AopConfig config;
+    /*
+    事务的管理对象也是在配置中，也可以从BeanDefinition中获取到
+    然后set实例化到代理对象中，然后在代理对象执行invocation方法中，
+    transactionManager进行事务开启、提交或回滚、关闭
+    private TransactionManager manager;
+    */
     //target为原生对象
     private Object target;
 
@@ -23,6 +29,8 @@ public class AopProxy implements InvocationHandler{
     }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+
         //获取原生对象的方法
         Method m = this.target.getClass().getMethod(method.getName(),method.getParameterTypes());
         //在原始方法调用以前要执行增强的代码
@@ -32,7 +40,12 @@ public class AopProxy implements InvocationHandler{
         }
         aspect.getPoints()[0].invoke(aspect.getAspect());
         //反射调用原始的方法
-        Object obj = method.invoke(this.target,args);
+        Object obj = null;
+        try{
+           obj  = method.invoke(this.target,args);
+        }catch (Exception e){
+
+        }
         //在原始方法调用以后要执行增强的代码
         aspect.getPoints()[1].invoke(aspect.getAspect());
         //将最原始的返回值返回出去
